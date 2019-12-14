3887decc80aacdf124c1252c55d92abdef3887bf
noorhashem <nour.hashem95@gmail.com>
2019-12-14 23:55:40 +0200

    edits in projects

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by McNoor's  on 12/8/19.
+//  Copyright © 2019 McNoor's . All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+}
---
+++ Smack/Smack/View/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by McNoor's  on 12/12/19.
+//  Copyright © 2019 McNoor's . All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
